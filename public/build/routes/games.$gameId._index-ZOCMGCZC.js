d1836bde9bd12d0b4d1f0c516187e90a11b176fe
Tim Harding <timharding@Tims-MacBook-Pro.local>
2025-03-18 19:26:50 -0700

    chore: Update gitignore and remove build files from git tracking

--- public/build/routes/games.$gameId._index-ZOCMGCZC.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"/build/_shared/chunk-5VJRENMX.js";import{d as t}from"/build/_shared/chunk-2QEWK57A.js";var o=t(r(),1);function e(){return null}export{e as default};
--- public/build/routes/ping-JU6B2AWL.js
+++
@@ -1,1 +0,0 @@
-import"/build/_shared/chunk-2QEWK57A.js";function e(){return null}export{e as default};
